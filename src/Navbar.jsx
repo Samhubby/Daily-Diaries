8d754259d74480a1ebb6e0ddb21b9d45881db71d
@Samhubby <samayaasylumml@gmail.com>
2024-03-29 18:22:42 +0545

    Navbar Modified

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className='bg-gray-600 px-7 py-3 flex justify-between items-center text-white'>
-        <span className='text-3xl cursor-pointer'><Link to='/'>CONFESS NOTE</Link></span>
+        <span className='text-3xl cursor-pointer'><Link to='/'>Daily Diaries</Link></span>
         <span className='text-xl cursor-pointer hover:text-gray-300'><Link to='/aboutus'>About Us</Link></span>
     </nav>
   )
