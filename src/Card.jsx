761cbc9ecf8f44067fc90fb52930214ec9ea21e3
@Samhubby <samayaasylumml@gmail.com>
2024-03-30 21:59:25 +0545

    Add Shimmering Effect

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,10 @@
+import { ShimmerSimpleGallery } from "react-shimmer-effects";
 import { database, onValue, ref } from "../config/firebase";
 import React, { useEffect, useState } from "react";
 
-
 const Card = () => {
   const [confessionList, setConfessionList] = useState([]);
+  const [isLoading, setisLoading] = useState(true);
   useEffect(() => {
     onValue(ref(database, "confessions/"), (snapshot) => {
       let data = snapshot.val();
@@ -12,27 +13,28 @@ const Card = () => {
         temp.push(data[key]);
       }
       setConfessionList(temp);
+      setisLoading(false);
     });
   }, []);
   return (
     <div className="flex w-screen flex-row items-center justify-center space-x-6 space-y-6 flex-wrap">
-      {confessionList.map((data, index) => {
-        return (
-          <div key={index}> 
-          <div className="border-2 border-purple-900 rounded-2xl w-60 h-60 p-2">
-            <div className="space-y-3">
-              <span className="text-sm">{data.date}</span>
-              <br />
-             
-              <p className="text-center">
-               {data.note}
-              </p>
-              <span className="text-sm"></span>
-            </div>
-          </div>
-          </div>
-        );
-      })}
+      {isLoading
+        ? <div className="w-[50%]"><ShimmerSimpleGallery card imageHeight={300} row={3} caption/></div>
+        : confessionList.map((data, index) => {
+            return (
+              <div key={index}>
+                <div className="border-2 border-purple-900 rounded-2xl w-60 h-60 p-2">
+                  <div className="space-y-3">
+                    <span className="text-sm">{data.date}</span>
+                    <br />
+
+                    <p className="text-center">{data.note}</p>
+                    <span className="text-sm"></span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
     </div>
   );
 };
